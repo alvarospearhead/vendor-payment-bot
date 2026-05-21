6be131357fc7aa920215a4a01e9e8ab905d4a4e0
Alvaro <alvaro@spearhead.construction>
2026-05-21 15:51:10 -0300

    Add Meta webhook verification

--- main.py
+++ main.py
@@ -2,10 +2,27 @@ from fastapi import FastAPI, Request
 
 app = FastAPI()
 
+VERIFY_TOKEN = "vendor_secret_123"
+
 
 @app.get("/")
 def home():
     return {"message": "Vendor Payment Bot Running"}
+
+
+@app.get("/webhook")
+async def verify_webhook(request: Request):
+
+    mode = request.query_params.get("hub.mode")
+    token = request.query_params.get("hub.verify_token")
+    challenge = request.query_params.get("hub.challenge")
+
+    if mode and token:
+
+        if mode == "subscribe" and token == VERIFY_TOKEN:
+            return int(challenge)
+
+    return {"error": "Verification failed"}
 
 
 @app.post("/webhook")
